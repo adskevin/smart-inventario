ade8599274e570a7dd7d5bd1f9da67bbac7e91ed
Kevin <kevin.souza.94@gmail.com>
2021-05-02 19:11:56 -0300

    Alteração na conexão com o postgres para permitir uso de ssl. Nescessário para heroku

--- config/database.js
+++ config/database.js
@@ -68,14 +68,19 @@ module.exports = {
   | npm i --save pg
   |
   */
+  // pg: {
+  //   client: 'pg',
+  //   connection: {
+  //     host: Env.get('DB_HOST', 'localhost'),
+  //     port: Env.get('DB_PORT', ''),
+  //     user: Env.get('DB_USER', 'root'),
+  //     password: Env.get('DB_PASSWORD', ''),
+  //     database: Env.get('DB_DATABASE', 'adonis')
+  //   }
+  // }
   pg: {
     client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+    connection: `postgres://${ Env.get('DB_USER', 'root') }:${ Env.get('DB_PASSWORD', '') }@${ Env.get('DB_HOST', 'localhost') }:${ Env.get('DB_PORT', '') }/${ Env.get('DB_DATABASE', 'adonis') }?ssl=true`,
+    healthCheck: false,
   }
 }
